aba47e37bb25d2d206faa9b36cccfc3280fa0489
MoniFrias <friasmoni18@gmail.com>
2023-11-01 13:03:46 -0600

    Api gateway

---
+++ api-gateway/src/test/java/com/mf/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mf.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
